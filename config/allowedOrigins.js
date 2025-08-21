25afebc5308be639583ee7aec912ed955e9a676a
Tristan Rinehart <tristan.rinehart@gmail.com>
2025-08-20 21:44:01 -0700

    trying to fix coors in prod

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -6,9 +6,9 @@ console.log(`Environment: ${environment}`);
 console.log(`Allowed Origins: ${process.env.ALLOWED_ORIGINS_DEV} | ${process.env.ALLOWED_ORIGINS_PROD}`);
 
 if (environment === 'development') {
-  allowedOrigins = process.env.ALLOWED_ORIGINS_DEV; // Load from environment variable
+  allowedOrigins = [process.env.ALLOWED_ORIGINS_DEV]; // Load from environment variable
 } else if (environment === 'production') {
-  allowedOrigins = process.env.ALLOWED_ORIGINS_PROD; // Load from environment variable
+  allowedOrigins = [process.env.ALLOWED_ORIGINS_PRO]; // Load from environment variable
 } else {
   // Default or throw an error for unsupported environments
   allowedOrigins = ['http://localhost:5173', 'https://custodian-frontend.onrender.com']; // Example default
