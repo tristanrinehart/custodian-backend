9b3f288c340819d1e4adb452814db4adfaf97055
Tristan Rinehart <tristan.rinehart@gmail.com>
2025-08-23 20:13:24 -0700

    dev ok, tweaks to make prod work

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,25 +2,17 @@
 const express = require('express');
 const router = express.Router();
 
-//const signin = require('./controllers/signin.controller');
 const refresh = require('./controllers/refreshtoken.controller');
 //const signout = require('./controllers/signout.controller');
 const { ensureCsrfCookie } = require('../middleware/csrf');
 
-// Sign in (sets cookies)
-//router.post('/signin', ensureCsrfCookie, signin);
-
 // Refresh access (reads jwt cookie; sets access cookie)
 router.get('/refresh', refresh);
 
-// Logout (clears cookies)
-//router.post('/logout', logout);
-
-// Optionally expose CSRF token to clients that want to prefetch it
-
+/* this should not be here
 router.get('/csrf-token', ensureCsrfCookie, (req, res) => {
   res.json({ csrfToken: req.csrfToken || req.cookies?.csrf || null });
 });
-
+*/
 
 module.exports = router;
